0d1d08830fdfbed98332a9176b00a0e0b5993609
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-01-02 14:28:07 -0800

    Initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jasper Tan on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
