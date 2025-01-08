072a493609e9e7ec6cd0238ab6929a8dcc9c3b7f
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2025-01-07 17:05:37 -0800

    Enable many filters for inputted photos with option to Share image

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -19,8 +19,10 @@ struct ContentView: View {
     
     @State private var selectedItem: PhotosPickerItem?
     
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var showingFilters = false
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
+
     
     var body: some View {
         
@@ -59,38 +61,66 @@ struct ContentView: View {
                     Button("Change filter") {
                         changeFilter()
                     }
+                    
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Filter Image", image: processedImage))
+                    }
                 }
+            
                 
-                Spacer()
             }
             .navigationTitle("Instafilter")
+            .confirmationDialog("Select a filter", isPresented: $showingFilters, actions: {
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+            })
             .padding([.horizontal, .bottom])
         }
     }
     
     func changeFilter() {
-        
+        showingFilters = true
     }
     
     func loadImage() {
-        Task {
-            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {
-                return
+            Task {
+                guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {
+                    return
+                }
+                
+                guard let inputImage = UIImage(data: imageData) else {
+                    return
+                }
+                
+                let beginImage = CIImage(image: inputImage)
+                
+                currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+                applyProcessing()
             }
-            
-            guard let inputImage = UIImage(data: imageData) else {
-                return
-            }
-            
-            let beginImage = CIImage(image: inputImage)
-            
-            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-            applyProcessing()
         }
-    }
     
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey)
+        {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey)
+        {
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey)
+        {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
         
         guard let outputImage = currentFilter.outputImage else {
             return
@@ -100,8 +130,13 @@ struct ContentView: View {
             return
         }
         
-        let uiImage = UIImage(cgImage: cgImage)
+        let uiImage = UIImage(cgImage: cgImage, scale: 1, orientation: .up)
         processedImage = Image(uiImage: uiImage)
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
